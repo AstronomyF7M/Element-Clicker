82b514a5a8a266609d211c343bdd2f95de51ecc0
AstronomyF7M <8769937516@student.cms.k12.nc.us>
2025-03-03 11:09:34 -0500

    Update and rename javascript.jse to javascript.js

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+// script.js
+let fireCount = 0;
+let waterCount = 0;
+let earthCount = 0;
+let airCount = 0;
+
+function clickElement(element) {
+    switch (element) {
+        case 'fire':
+            fireCount++;
+            document.getElementById('fireCount').innerText = fireCount;
+            break;
+        case 'water':
+            waterCount++;
+            document.getElementById('waterCount').innerText = waterCount;
+            break;
+        case 'earth':
+            earthCount++;
+            document.getElementById('earthCount').innerText = earthCount;
+            break;
+        case 'air':
+            airCount++;
+            document.getElementById('airCount').innerText = airCount;
+            break;
+    }
+}
+
+function upgrade() {
+    // Upgrade logic can be implemented here
+    alert("Upgrade feature coming soon!");
+}
